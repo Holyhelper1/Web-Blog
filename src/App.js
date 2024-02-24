a1954e55c8d16e02685e9ab9f2c3f4f25dc92946
NemesisWar <122649498+Holyhelper1@users.noreply.github.com>
2024-02-24 12:41:21 +0300

    Описание данных

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 const Div = styled.div`
-	color: red;
+	color: #4B0082;
 	text-align: center;
 	text-shadow: 2px 2px 4px #004050;
+	background-color: #C0C0C0;
 `;
 
 function App() {
   return (
     <Div >
-		<i className="fa fa-calendar"></i>
+		<i className="fa fa-calendar fa-4x"> календарь заданий</i>
       <h1>Hello, World 123!</h1>
     </Div>
   );
